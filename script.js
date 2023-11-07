d705afe8c295bee5232219dd5e4cdd6b9f717288
Konstantin <kostya1703tsoy@gmail.com>
2023-11-07 20:28:56 +0300

    history

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById("btn");
 const sidebar = document.querySelector(".sidebar");
+const historyBottom = document.querySelector(".history-bottom")
 
 btn.addEventListener("click", function() {
     sidebar.classList.toggle("active");
+    historyBottom.classList.toggle("active");
 })
